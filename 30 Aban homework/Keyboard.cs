05451ff2be103073a14eeca57f6f9ef95bbd1e59
Sepehr Rasouli <sepehrrasoolishamsi@gmail.com>
2024-11-26 15:49:40 +0330

    Update Keyboard.cs

--- 30 Aban homework/Keyboard.cs
+++ 30 Aban homework/Keyboard.cs
@@ -4,7 +4,7 @@ public class Keyboard{
         bool ToContinue = true;
         while(ToContinue){
             Console.WriteLine($"> {String.Join("",Input)}");
-            System.ConsoleKeyInfo input = Console.ReadKey();
+            System.ConsoleKeyInfo input = System.Console.ReadKey();
             if (input.Key.Equals(ConsoleKey.Enter) && Input.Count > 0){
                 ToContinue = false;
                 Console.Clear();
@@ -29,18 +29,36 @@ public class Keyboard{
     }
 
     public static int GetNumber(){
+        List<string> NumbersList = new List<string>(){};
         bool ToContinue = true;
-        string ValidatedCharacter = "";
         while(ToContinue){
-            System.ConsoleKey input = Console.ReadKey(true).Key;
-            if(!"01".Contains(input.ToString().ToLower())){
+            Console.WriteLine($"> {String.Join("",NumbersList)}");
+            System.ConsoleKeyInfo input = Console.ReadKey();
+            if(char.IsDigit(input.KeyChar)){
+                NumbersList.Add(input.KeyChar.ToString());
+                Console.Clear();
                 continue;
             }
-            ToContinue = false;
-            ValidatedCharacter = input.ToString().ToLower();
+            if (input.Key.Equals(ConsoleKey.Enter) && NumbersList.Count > 0){
+                ToContinue = false;
+                Console.Clear();
+                break;
+            }
+            if (input.Key.Equals(ConsoleKey.Backspace)){
+                if (NumbersList.Count == 0){
+                    continue;
+                }
+                NumbersList.RemoveAt(NumbersList.Count-1);
+                Console.Clear();
+                continue;
+            }
+            else{
+                Console.Clear();
+                continue;
+            }
         }
-        return Convert.ToInt16(ValidatedCharacter);
-    }   
+        return Convert.ToInt32(String.Join("",NumbersList));
+    }
 
     public static string GetFileExtension(){
         System.Console.WriteLine("Enter the type of file to write data to [ini,txt,csv]> ");
