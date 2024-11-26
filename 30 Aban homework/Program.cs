c060daf3f047a3203290583030c490cae033cb1f
Sepehr Rasouli <sepehrrasoolishamsi@gmail.com>
2024-11-26 15:49:01 +0330

    Update Program.cs

--- 30 Aban homework/Program.cs
+++ 30 Aban homework/Program.cs
@@ -1,4 +1,4 @@
-﻿void main(){
+void main(){
     Console.Write("Enter Sender's name > ");
     string Sender = Keyboard.GetInput();
 
@@ -14,6 +14,7 @@
     Encode encode = new Encode(Sender,Reciever,Message,Type);
     Tuple<string,string> EncodedData = encode.EncodeData();
     Decode decode = new Decode(Sender,Reciever,EncodedData.Item1,EncodedData.Item2);
+    string DecodedData = decode.DecodeData();
 
     System.Console.WriteLine($"The encoded message is {EncodedData.Item1}");
     System.Console.WriteLine($"The decoded message is {decode.DecodeData()}");
@@ -21,17 +22,20 @@
     string FileExtension = Keyboard.GetFileExtension();
     if (FileExtension == "txt"){
         TxtWriter txtWriter = new TxtWriter();
-       txtWriter.WriteText(EncodedData.Item1,$"encoded.{FileExtension}");
+        txtWriter.WriteText(EncodedData.Item1,$"encoded.{FileExtension}");
+        txtWriter.WriteText(DecodedData,$"encoded.{FileExtension}");
     }
     else if (FileExtension == "ini"){
         IniWriter iniWriter = new IniWriter();
         iniWriter.WriteText(EncodedData.Item1,$"encoded.{FileExtension}");
+        iniWriter.WriteText(DecodedData,$"decoded.{FileExtension}");
     }
     else{
         CsvWriter csvWriter = new CsvWriter();
         csvWriter.WriteText(EncodedData.Item1,$"encoded.{FileExtension}");
+        csvWriter.WriteText(DecodedData,$"decoded.{FileExtension}");
     }
 
     System.Console.WriteLine($"The decoded and the encoded message were written to decoded.{FileExtension} and encoded.{FileExtension}");
 }
-main();
+main(); 
